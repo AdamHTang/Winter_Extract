336150ce150fdb64c77e4facfba4913e41bb1d27
Adam Tang <aht3138@utulsa.edu>
2021-09-26 21:18:09 -0500

    Added Soldier Assets and Scripts
    Added soldier assets (free on Unity Store)
    Added health script and player controller script.

---
+++ Tang_Winter-Extract/Assets/Scripts/Health.cs
@@ -0,0 +1,55 @@
+/*
+ *  Created by: Adam Tang
+ *  Date Created: Sept 20, 2021
+ *  
+ *  Last Edited by:
+ *  Last Updated: Sept 26, 2021
+ *  
+ *  Description: Game object health
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    // Variables //
+    [SerializeField] private float _HealthPoints = 100f;
+    public bool DestroyOnDeath = true;
+    public GameObject DeathPrefab = null;
+    Animator anim;
+
+    public float HealthPoints
+    {
+        get { return _HealthPoints; } // End get()
+        set
+        {
+            _HealthPoints = value;
+            if (HealthPoints <= 0)
+            {
+                SendMessage("Die", SendMessageOptions.DontRequireReceiver);
+                
+                if (DeathPrefab != null)
+                {
+                    Instantiate(DeathPrefab, transform.position,Quaternion.Euler(90,transform.rotation.y,0));
+                }
+
+                if (DestroyOnDeath)
+                {
+                    Destroy(gameObject);
+                }
+            }
+
+        } // End set()
+    } // End HealthPoints()
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug Health Test
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            HealthPoints = 0;
+        } // End Debug
+    }
+}
---
+++ Tang_Winter-Extract/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+/*
+ *  Created by: Adam Tang
+ *  Date Created: Sept 26, 2021
+ *  
+ *  Last Edited by:
+ *  Last Updated: Sept 26, 2021
+ *  
+ *  Description: Player Controller that controls movement and shooting.
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    /* VARIABLES */
+    public bool mouseLook = true;           // Are we looking at the mouse.
+    public string horzAxis = "Horizontal";
+    public string vertAxis = "Vertical";
+    public float speed = .1f;             // Speed of the character.
+    private Rigidbody ThisBody = null;
+    Vector3 moveDirection;
+
+    public string FireAxis = "Fire1";
+    public float ReloadDelay = 0.0f;
+    public bool CanFire = true;
+    public Transform[] TurretTransforms;
+
+    /* Awake - Call before game start*/
+    private void Awake()
+    {
+        ThisBody = GetComponent<Rigidbody>();        // Sets the object's rigidbody to a variable called playerRB.
+    }   // End Awake()
+
+
+    private void FixedUpdate()
+    {
+        moveDirection.x= Input.GetAxisRaw(horzAxis);
+        moveDirection.z= Input.GetAxisRaw(vertAxis);
+    
+
+        ThisBody.MovePosition(ThisBody.position + (moveDirection.normalized * speed));  
+
+        if (mouseLook)
+        {
+            Vector3 mousePosWorld = Camera.main.ScreenToWorldPoint(
+                                  new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0.0f));
+
+            mousePosWorld = new Vector3(mousePosWorld.x, 0.0f, mousePosWorld.z);
+
+            Vector3 lookDirection = mousePosWorld - transform.position;
+            transform.localRotation = Quaternion.LookRotation(lookDirection.normalized, Vector3.up);
+
+        }
+
+    }
+}
